a69aac013198ae38a992d445474a91284040b083
DK <dengke@yappam.com>
2018-09-29 15:05:30 +0800

    添加数据库

--- practise_app/app.py
+++ practise_app/app.py
@@ -1,6 +1,19 @@
+# -coding:utf-8-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import os
 
+#获取当前文件的绝对路径
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///'+ os.path.join(basedir,'model_db.db')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] =True
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.session_protection = 'strong'
+login_manager.login_view = '/'
 
 
 @app.route('/')
---
+++ practise_app/model/__init__.py
@@ -0,0 +1,6 @@
+#--coding:utf-8--
+from app import db
+from model.models import User
+
+#生成数据库
+# db.create_all()
