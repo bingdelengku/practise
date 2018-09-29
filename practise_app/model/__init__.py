99ed7a2b7b9c58cb7a5e3578410ce92652aab99e
DK <dengke@yappam.com>
2018-09-29 16:26:09 +0800

    增加文章模型

--- practise_app/model/__init__.py
+++ practise_app/model/__init__.py
@@ -1,6 +1,6 @@
 #--coding:utf-8--
-from app import db
-from model.models import User
+# from app import db
+# from model.models import User
 
 #生成数据库
 # db.create_all()
--- practise_app/model/models.py
+++ practise_app/model/models.py
@@ -36,4 +36,72 @@ class User(UserMixin,db.Model):
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+#文章分类
+class Category(db.Model):
+    __tablename__ = 'category'
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(50))
+
+    def __init__(self,name):
+        self.name = name
+    def __repr__(self):
+        return '<Category %r>' % self.name
+
+#文章
+class Articles(db.Model):
+    __tablename__ = 'articles'
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(80))
+    author = db.Column(db.String(80))
+    content = db.Column(db.Text)
+    pub_date = db.Column(db.DateTime)
+    reading_number = db.Column(db.Integer)
+
+    category_id = db.Column(db.Integer,db.ForeignKey('category.id'))
+    category = db.relationship('Category',backref=db.backref('articles',lazy='dynamic'))
+
+    def __init__(self,title,content,reading_number,category,pub_date=None,author='DK'):
+        self.title = title
+        self.author = author
+        self.content = content
+        self.reading_number = reading_number
+        self.category = category
+        if pub_date is None:
+            pub_date = datetime.utcnow()
+        self.pub_date = pub_date
+
+    def __repr__(self):
+        return '<Articles %r>' % self.title
+
+
+#文章标签
+class Tag(db.Model):
+    __tablename__ = 'tag'
+    id = db.Column(db.Integer,primary_key=True)
+    article_id = db.Column(db.Integer)
+    name = db.Column(db.String(50))
+
+    def __init__(self,article_id,name):
+        self.article_id = article_id
+        self.name = name
+
+    def __repr__(self):
+        return '<Tag %r>' % self.name
+
+
+#文章和标签映射表
+class ActMappTags(db.Model):
+    __tablename__ = 'actmapptags'
+    id = db.Column(db.Integer,primary_key=True)
+    article_id = db.Column(db.Integer)
+    tag_id = db.Column(db.Integer)
+
+    def __init__(self,article_id,tag_id):
+        self.article_id = article_id
+        self.tag_id = tag_id
+
+    def __repr__(self):
+        return '<ActMappTags %r>' % self.article_id
+
+
 
